21d223943847998b33a6398656b8d5b888f0c546
Quinian <41967524+Quinian@users.noreply.github.com>
2025-08-29 16:32:26 -0500

    Adding the main fold to prevent zip in zip

---
+++ Dragon Warrior/scripts/autotracking/location_mapping.lua
@@ -0,0 +1,109 @@
+local LOCATION_MAPPING = {
+
+    -- Level Up Rewards (Levelsanity)
+--    [7200] = "@Main/Levels/Level 1", don't really need this one
+    [7201] = "@Main/Levels/Level 2",
+    [7202] = "@Main/Levels/Level 3",
+    [7203] = "@Main/Levels/Level 4",
+    [7204] = "@Main/Levels/Level 5",
+    [7205] = "@Main/Levels/Level 6",
+    [7206] = "@Main/Levels/Level 7",
+    [7207] = "@Main/Levels/Level 8",
+    [7208] = "@Main/Levels/Level 9",
+    [7209] = "@Main/Levels/Level 10",
+    [7210] = "@Main/Levels/Level 11",
+    [7211] = "@Main/Levels/Level 12",
+    [7212] = "@Main/Levels/Level 13",
+    [7213] = "@Main/Levels/Level 14",
+    [7214] = "@Main/Levels/Level 15",
+    [7215] = "@Main/Levels/Level 16",
+    [7216] = "@Main/Levels/Level 17",
+    [7217] = "@Main/Levels/Level 18",
+    [7218] = "@Main/Levels/Level 19",
+    [7219] = "@Main/Levels/Level 20",
+    [7220] = "@Main/Levels/Level 21",
+    [7221] = "@Main/Levels/Level 22",
+    [7222] = "@Main/Levels/Level 23",
+    [7223] = "@Main/Levels/Level 24",
+    [7224] = "@Main/Levels/Level 25",
+    [7225] = "@Main/Levels/Level 26",
+    [7226] = "@Main/Levels/Level 27",
+    [7227] = "@Main/Levels/Level 28",
+    [7228] = "@Main/Levels/Level 29",
+    [7229] = "@Main/Levels/Level 30",
+
+    -- Major Chests and Map Checks
+
+    [7230] = "@Main/Tantagel Castle/Throne Room - Gold Chest",
+    [7231] = "@Main/Tantagel Castle/Throne Room - Key Chest",
+    [7232] = "@Main/Tantagel Castle/Throne Room - Torch Chest",
+    [7233] = "@Main/Tantagel Castle/Tantegel Castle - Gold Chest 1",
+    [7234] = "@Main/Tantagel Castle/Tantegel Castle - Gold Chest 2",
+    [7235] = "@Main/Tantagel Castle/Tantegel Castle - Gold Chest 3",
+    [7236] = "@Main/Tantagel Castle/Tantegel Castle - Gold Chest 4",
+    [7280] = "@Main/Tantagel Castle/Stones of Sunlight",
+    [7901] = "@Main/Tantagel Castle/Returned Gwaelin to Castle",
+
+    [7900] = "@Main/Swamp Cave/Rescue Princess Gwaelin",
+
+    [7237] = "@Main/Garinham/Garinham - Chest 1",
+    [7238] = "@Main/Garinham/Garinham - Chest 2",
+    [7239] = "@Main/Garinham/Garinham - Chest 3",
+    [7247] = "@Main/Garin's Grave/Garin's Grave - Level 1 - Chest 1",
+    [7248] = "@Main/Garin's Grave/Garin's Grave - Level 1 - Chest 2",
+    [7249] = "@Main/Garin's Grave/Garin's Grave - Level 1 - Chest 3",
+    [7250] = "@Main/Garin's Grave/Garin's Grave - Level 3 - Chest 1",
+    [7251] = "@Main/Garin's Grave/Garin's Grave - Level 3 - Chest 2",
+
+    [7240] = "@Main/Kol/Fairy Flute",
+
+    [7241] = "@Main/Rimuldar/Rimuldar - Inn Chest",
+
+    [7242] = "@Main/Mountain Cave/Mountain Cave - Level 1 - Chest",
+    [7243] = "@Main/Mountain Cave/Mountain Cave - Level 2 - Chest 1",
+    [7244] = "@Main/Mountain Cave/Mountain Cave - Level 2 - Chest 2",
+    [7245] = "@Main/Mountain Cave/Mountain Cave - Level 2 - Chest 3",
+    [7246] = "@Main/Mountain Cave/Mountain Cave - Level 2 - Chest 4",
+
+    [7262] = "@Main/Charlock Castle/Erdrick's Sword",
+    [7252] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 1",
+    [7253] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 2",
+    [7254] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 3",
+    [7255] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 4",
+    [7256] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 5",
+    [7257] = "@Main/Charlock Castle/Dragonlord's Lair - Chest 6",
+    [7258] = "@Main/Charlock Castle/Defeated Dragonlord",
+
+    [7263] = "@Main/Hauksness/Erdrick's Armor",
+
+    -- World Map
+
+    [7259] = "@Main/world_map/Rainbow Shrine",
+    [7260] = "@Main/world_map/Staff of Rain",
+    [7261] = "@Main/world_map/Erdrick's Token",
+
+    [7264] = "@Main/Erdrick's Cave/Erdrick's Tablet",
+
+    -- Shopsanity
+    [7265] = "@Main/Shopsanity/Purchased: Bamboo Pole",
+    [7266] = "@Main/Shopsanity/Purchased: Club",
+    [7267] = "@Main/Shopsanity/Purchased: Copper Sword",
+    [7268] = "@Main/Shopsanity/Purchased: Clothes",
+    [7269] = "@Main/Shopsanity/Purchased: Leather Armor",
+    [7270] = "@Main/Shopsanity/Purchased: Small Shield",
+    [7271] = "@Main/Shopsanity/Purchased: Chain Mail",
+    [7272] = "@Main/Shopsanity/Purchased: Large Shield",
+    [7273] = "@Main/Shopsanity/Purchased: Hand Axe",
+    [7274] = "@Main/Shopsanity/Purchased: Half Plate",
+    [7275] = "@Main/Shopsanity/Purchased: Full Plate",
+    [7276] = "@Main/Shopsanity/Purchased: Broad Sword",
+    [7277] = "@Main/Shopsanity/Purchased: Magic Armor",
+    [7278] = "@Main/Shopsanity/Purchased: Flame Sword",
+    [7279] = "@Main/Shopsanity/Purchased: Silver Shield",
+
+
+
+}
+
+
+return LOCATION_MAPPING
